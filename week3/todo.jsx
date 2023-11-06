15cf852108744401d2f8102d60826a06d64916d1
Maryam Riaz <104584580+Mariyamriaz@users.noreply.github.com>
2023-11-06 19:00:48 +0500

    todo list

---
+++ week3/todo.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+ const [todos,setTodo]=useState([]);
+ const [task,setTask]=useState("");
+ const [status,setStatus]=useState("INCOMPLETE");
+
+
+  return (
+    <>
+      
+     <form onSubmit={(e)=>
+    {
+e.preventDefault;
+setTodo([...todos,
+{
+  task:task,
+  status:status;
+}])
+
+    }}>
+      <label> Task</label>
+      <input  
+      required
+      type="text"
+      value={task}
+      onChange={function(e){
+      setTask(e.target.value);
+      }} />
+      <select 
+      required
+      value={status}
+      onChange={function(e){
+      setStatus(e.target.value);
+      }}>
+        
+        <option value="completed">completed</option>
+        <option value="incomplete">incomplete</option>
+      </select>
+      <button type='submit'>submit</button>
+    </form>
+    <div>
+      {todos.map((item)=>
+      {
+        return(
+          <div key={item}>
+            <span>{item.task}</span>
+            <span>{item.status}</span>
+          </div>
+        )
+      })}
+    </div>
+    
+
+    </>
+  )
+}
+
+export default App
