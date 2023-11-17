73fa080d8f9ff2c85a6084416e44fe62286640ee
Maryam Riaz <104584580+Mariyamriaz@users.noreply.github.com>
2023-11-17 18:38:32 +0500

    progress done

--- week4/CompanyTaskAssignment/Task.jsx
+++ week4/CompanyTaskAssignment/Task.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
 
 export default function Tasks() {
-    const [task,setTask] = useState();
+    const [task,setTask] = useState("");
+    const [show,setShow] =useState([]);
+    
 
   return (
     <>
     
     <div>Tasks</div>
-    <form>
+    <form onSubmit={(e)=>
+    {
+     e.preventDefault();
+     setShow([...show,
+    {  
+     task:task
+     
+    }
+    ]);
+        
+}}>
+
         <label>Task</label>
         <input 
         className='border-2 '
@@ -21,10 +34,33 @@ export default function Tasks() {
         />
         <button
         className='bg-gray-400 rounded-md m-5 p-2'
-        onSubmit={(e)=> e.preventDefault()}>
+        onSubmit={(e)=> {e.preventDefault()}}>
             Submit
         </button>
+
+        <button 
+        className='bg-gray-400 rounded-md m-5 p-2'>
+            Remove
+        </button>
+
     </form>
+    <div>
+      {show.map((item)=>
+      {
+        return(
+          <div key={item}>
+            <span>{item.task}  </span>
+            <span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+</svg>
+
+            </span>
+            
+          </div>
+        );
+      })}
+    </div>
   
     </>
   )
